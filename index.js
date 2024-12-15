3195c36359d5ac49434482d9515604b06c442db4
sumonDev9 <sumonmit678@gmail.com>
2024-12-15 21:25:21 +0530

    Added a /jobs API endpoint to fetch and return all jobs from the database

--- index.js
+++ index.js
@@ -26,9 +26,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+   // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     
+    // jobs related api
+    const jobsCollection = client.db('careerConnect').collection('jobs');
+
+    app.get('/jobs', async (req, res) => {
+        const result = await jobsCollection.find().toArray();
+        res.send(result)
+    })
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
